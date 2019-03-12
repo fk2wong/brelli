160ae44d0824215a4c6ebc41816a8f780c875c75
Sejin Kwon <sejin.kwon10@gmail.com>
2019-03-12 18:21:53 -0400

    Simultaneous IMU and photodiode reading

--- brelli.ino
+++ brelli.ino
@@ -1,17 +1,26 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "IMU.h"
+#include "PhotodiodeArray.h"
 
 #define LOOP_MS (5)
-#define PRINT_IMU
+#define PRINT_VALUES
+
+const int frameReadyPin = A3;
+const int chipSelectPin = 10;
+
+PhotodiodeArray array1;
+unsigned int integrationTime = 0x4E24;
 
 IMU imu;
 
 void setup(){
+  SPI.begin();
   Wire.begin();
   Serial.begin(9600);
   
   imu.init(0, true, 0.18);
+  array1.init(chipSelectPin, frameReadyPin);
 }
 
 void loop(){
@@ -19,13 +28,14 @@ void loop(){
   
   imu.update(current, raw);
 
-#ifdef PRINT_IMU
+#ifdef PRINT_VALUES
   static int readCount = 0;
   if ((readCount % 25) == 0)
   {
     Serial.print("Roll:\t"); Serial.print(current.roll); Serial.print("\t"); Serial.print(raw.roll);
     Serial.print("\t\tPitch:\t"); Serial.print(current.pitch); Serial.print("\t"); Serial.print(raw.pitch);
     Serial.println();
+    Serial.print("Photodiode array centroid: "); Serial.println(array1.getCentroidReading(integrationTime));
   }
   readCount++;
 #endif
